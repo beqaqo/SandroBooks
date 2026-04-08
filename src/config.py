68b1bb9f46fae1b1d5464835dbd7355d090ccfd5
beqaqo <beka.kopadze.2@iliauni.edu.ge>
2026-04-08 15:43:36 +0400

    added postgres to configuration

--- src/config.py
+++ src/config.py
@@ -2,6 +2,17 @@ from os import path, environ
 
 class Config(object):
     BASE_DIRECTORY = path.abspath(path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = "sqlite:///books.db"
+
+    DATABASE_URL = environ.get("DATABASE_URL")
+
+    if DATABASE_URL:
+        # Fix Render postgres:// issue
+        if DATABASE_URL.startswith("postgres://"):
+            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        # fallback for local development
+        SQLALCHEMY_DATABASE_URI = "sqlite:///books.db"
+
     SECRET_KEY = environ.get("SECRET_KEY", "defaultsecretkey")
     UPLOAD_PATH = path.join(BASE_DIRECTORY, "static", "assets")
