a5204a382f6146840534e99c27b7719cc3a24a37
Luka <luka.natsvlishvili.1@btu.edu.ge>
2026-04-19 17:41:54 +0400

    Add SeriesView to admin panel

--- src/admin_views/__init__.py
+++ src/admin_views/__init__.py
@@ -1,5 +1,8 @@
 from src.admin_views.book import BookView
+from src.admin_views.series import SeriesView
 
 def register_admin_views(admin, db):
     from src.models.book import Book
+    from src.models.series import Series
     admin.add_view(BookView(Book, db.session))
+    admin.add_view(SeriesView(Series, db.session))
